8bfabbcbb4dc23c19dc91fec643b606d0cc11a75
student <student@eda-bc-04.localdomain>
2020-01-30 17:03:28 +1300

    Home route added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,11 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <Route path='/' component={Home}/>
+      </div>
+    </Router>
   )
 }
 
