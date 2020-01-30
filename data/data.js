0ad1513c1f0b2e27f305620d82791cefffd11f3d
Student <student@eda-bc-05.localdomain>
2020-01-31 11:26:44 +1300

    position footer

--- data/data.js
+++ data/data.js
@@ -14,8 +14,8 @@ const data = {
   },
   Bikes : {
     name: 'Mountain bikes, road bikes, all bikes',
-    image: "https://skoda-wlc.s3.amazonaws.com/2/2016/09/AudreyHepburn1954.jpg",
-    secondImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwGsJ5Dmh6J3E2VATOKoEoBKLxLBvX-uWUM_bqYTTG6xP88fHf&s",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRelhcxPryJbqbkZr31FWxbuoGzjDwIpCfgR2sstAh_xHIHIZFh",
+    secondImage: "https://skoda-wlc.s3.amazonaws.com/2/2016/09/AudreyHepburn1954.jpg",
     description: "Bike bike i'm a bike, vroom vroom not a car! The first bike was created in 1817 and was called the running machine.",
     gif: "https://designmodo.com/wp-content/uploads/2015/08/wheels.gif",
   }
