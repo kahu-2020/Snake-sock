f6d8bb19b1e5c2840863b8f313655ccff4069fee
Ethan-Meleah <40743637+Ethan-Meleah@users.noreply.github.com>
2020-01-31 10:31:52 +1300

    ayo

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
+//Data Import
+import data from '../../data/data'
+
+//Component Import
+import Trixie from './Trixie'
+
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <Route path="/trixie" render={(props) => <Trixie {...props} trixie={data.Trixie} />} />
+    </Router>
   )
 }
 
--- data/data.js
+++ data/data.js
@@ -1,6 +1,12 @@
 const data = {
   Trixie : {
-
+    name: 'Trixie Mattel',
+    headerUrl: 'https://www.trixiemattel.com/wp-content/uploads/2019/10/title-hero-updated.png',
+    bio: 'Trixie Mattel is the stage name of Brian Michael Firkus, an American singer, actor, drag queen and recognized “Skinny Legend.” Trixie is the winner of “RuPaul’s Drag Race All Stars” season 3 (originally competing on season 7). He is also a Billboard Heatseekers #1 charting recording artist (One Stone, 2018), a wildly successful theater touring act, and one half of a comic duo on Viceland (“The Trixie and Katya Show”) and on YouTube (“UNHhhh”). In 2019, Trixie will be working the festival circuit as the subject of a bio documentary, “Trixie Mattel: Moving Parts.” Trixie Mattel is managed by Producer Entertainment Group and online at trixiemattel.com or @trixiemattel.',
+    mainImages: ['https://i.redd.it/heluvobhly131.jpg', 'https://vignette.wikia.nocookie.net/logosrupaulsdragrace/images/9/9d/TrixieAS3.jpg/revision/latest?cb=20171021010404', 'https://img.buzzfeed.com/buzzfeed-static/static/2018-10/10/13/asset/buzzfeed-prod-web-03/sub-buzz-19424-1539194180-1.jpg', 'https://www.trixiemattel.com/wp-content/uploads/2019/10/grown-up-hero-mobile-2.jpg'],
+    headerImage: 'https://www.trixiemattel.com/wp-content/uploads/2019/10/title-hero-updated.png',
+    makeupImage: 'https://www.trixiecosmetics.com/static/44bc80b86a114a38cbb00dafbf5e11aa/8f517/header-desktop.png',
+    menuImage: 'https://www.trixiecosmetics.com/static/footer-logo-e3c6965c6c6cec39515477409d024fe1.png'  
   },
   Santino : {
 
@@ -10,3 +16,5 @@ const data = {
   }
 
 }
+
+export default data
