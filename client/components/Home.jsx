e328bce402582175b657d8b6e3fab298e01a5ddc
Student <student@eda-bc-05.localdomain>
2020-01-31 14:28:00 +1300

    className added

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom'
 const Home = () => {
     return (
         <div>
-            <h1>How curious are you?</h1>
-            <div>
-              <button><Link to={'/Bikes'}>claire</Link></button>
-              <button><Link to={'/Santino'}>lui</Link></button>
-              <button><Link to={'/Trixie'}>ethan</Link></button>
+            <h1>Mystery Box?</h1>
+            <div className='homeButtons'>
+              <button><Link to={'/Bikes'}>?</Link></button>
+              <button><Link to={'/Santino'}>??</Link></button>
+              <button><Link to={'/Trixie'}>???</Link></button>
             </div>
 
         </div>
