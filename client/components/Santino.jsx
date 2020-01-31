61849b8ca64659441b64d2999e11d05695d24304
lui-kelekolio <57427805+lui-kelekolio@users.noreply.github.com>
2020-01-31 14:51:16 +1300

    lastpush

--- client/components/Santino.jsx
+++ client/components/Santino.jsx
@@ -5,7 +5,7 @@ class Santino extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-           image_path : this.props.santino.footerImage1  
+           image_path : this.props.santino.mainImages
         }
 
         this.handleClick = this.handleClick.bind(this)
@@ -22,20 +22,27 @@ class Santino extends React.Component {
     render () {
         return (
             <div>
+                <div className="title">
+                <h2> {this.props.santino.name} </h2>
+                </div>
+                <div className="Logo">
+                    <img  src={this.props.santino.headerImage} height='250' width='250'/>
+                    </div>
                 <div className="mainPhoto santinoMainPhoto"> 
                     <img src={this.props.santino.mainImages} height='600'  /> 
                 </div>
                 <section className="mainWrapper MainWrapper">
+                   
                     
-                    <img  src={this.props.santino.headerImage} height='250' width='250'/>
-                    
-                    <h2> {this.props.santino.name} </h2>
 
-                    <article className="mainContent santinoMainContent">
-                        <p> Arguably the greatest WWE superstar of all time, Santino Marella holds many records and achievements within the squared octagon.Ever since debuting on the 16th of April in 2007, hes been without a doubt the biggest star in WWE history since Eugene back in 2004 (despite Eugene failing 2 drug tests and being fired from the WWE).He is a two-time WWE Intercontinental Champion, one-time WWE United States Champion, and one-time WWE Tag Team Champion. </p>
+                    <article className="santinoMainContent">
+                        <p> Arguably the greatest WWE superstar of all time, Santino Marella holds many records and achievements within the squared octagon. Ever since debuting on the 16th of April in 2007, hes been without a doubt the biggest star in WWE history since Eugene back in 2004 (despite Eugene failing 2 drug tests and being fired from the WWE).He is a two-time WWE Intercontinental Champion, one-time WWE United States Champion, and one-time WWE Tag Team Champion. </p>
                     </article>
-
-                    <img src={this.state.image_path} onClick={this.handleCLick}/>
+                <div className="footerImage" >
+                    <img src={this.props.santino.footerImage1}height='200' width='400'/>
+                    <img src={this.props.santino.footerImage2}height='250' width='400'/>
+                    <img src={this.props.santino.footerImage3}height='200' width='400'/>
+                </div>
                 </section>
             </div>
         )
--- data/data.js
+++ data/data.js
@@ -34,6 +34,7 @@ const data = {
     headerUrl:'',
     bio:'Arguably the greatest WWE superstar of all time, Santino Marella holds many records and achievements within the squared octagon.Ever since debuting on the 16th of April in 2007, hes been without a doubt the biggest star in WWE history since Eugene back in 2004 (despite Eugene failing 2 drug tests and being fired from the WWE).He is a two-time WWE Intercontinental Champion, one-time WWE United States Champion, and one-time WWE Tag Team Champion.',
     mainImages:'https://upload.wikimedia.org/wikipedia/commons/5/51/Santino_Marella_2010_Tribute_to_the_Troops.jpg',
+    //mainImage2:'https://i.pinimg.com/236x/2e/d6/f6/2ed6f6edc9e390cbc6ce9da1742c391f--santino-marella-pirelli.jpg',
     headerImage:'https://1000logos.net/wp-content/uploads/2017/05/WWE-logo.png',  
     footerImage1: 'https://vignette.wikia.nocookie.net/wmmainevent/images/d/d3/Wwe-intercontinental-championship-98-11.png/revision/latest?cb=20170715035538',
     footerImage2: 'https://upload.wikimedia.org/wikipedia/en/d/d4/World_Tag_Team_Championship.png',
